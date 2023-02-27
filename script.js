cb61a1a4bafe16efee78ced43368efaaea4cf60b
Lauren Giordano <laurengiordano94@gmail.com>
2023-02-28 10:42:07 +1100

    added link to resume, and content + layout adjustments

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
-const h2 = document.createElement("h2");
+/* const h2 = document.createElement("h2");
 h2.textContent = "This content added by Javascript";
 
-document.querySelector("body").appendChild(h2);
+document.querySelector("body").appendChild(h2); */
+
+/*
+const gridWork = document.getElementById("gridwork");
+
+gridWork.addEventListener("mouseover", (event) => {
+    event.target.style.background = "pink";
+    setTimeout(() => {
+        event.target.style.background = "";
+    }, 500);
+}, false);
+*/
+
+/*
+
+function mouseOver() {
+    document.getElementById("box1").style.background = "pink";
+};
+
+function mouseOver() {
+    document.getElementById("box2").style.background = "#FFB6C1";
+};
+
+function mouseOver() {
+    document.getElementById("box3").style.background = "#FF69B4";
+};
+
+function mouseOver() {
+    document.getElementById("box4").style.background = "#FF1493";
+};
+
+/*
+function.mouseOut() {
+    document.getElementById("gridwork").style.background = "blue";
+}
+
+*/
